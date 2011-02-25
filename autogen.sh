9f0891051d715a7ca6e555073f8f5fd49f3eae26
Florian Rivoal <frivoal@xfce.org>
2011-02-25 09:05:30 +0900

    Use recent xdt to detect available translations

--- autogen.sh
+++ autogen.sh
@@ -1,23 +1,20 @@
 #!/bin/sh
 #
-# $Id$
-#
-# Copyright (c) 2002-2005
+# Copyright (c) 2002-2009
 #         The Xfce development team. All rights reserved.
 #
 # Written for Xfce by Benedikt Meurer <benny@xfce.org>.
-#
+
+export XDT_AUTOGEN_REQUIRED_VERSION="4.7.0"
 
 (type xdt-autogen) >/dev/null 2>&1 || {
   cat >&2 <<EOF
 autogen.sh: You don't seem to have the Xfce development tools installed on
             your system, which are required to build this software.
-            Please install the xfce4-dev-tools package first, it is available
-            from http://www.xfce.org/.
+            Please install the xfce4-dev-tools package first, available from
+            http://xfce.org/~benny/projects/xfce4-dev-tools/.
 EOF
   exit 1
 }
 
-exec xdt-autogen $@
-
-# vi:set ts=2 sw=2 et ai:
+xdt-autogen $@
