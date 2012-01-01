xfce-mirror/xfce4-genmon-plugin

commit 5d9618659709b820c73b547b335a8c0c160264ca

Reset the timer after changing properties
Fixes bug #4050.

panel-plugin/main.c
@@ -585,8 +585,12 @@ static void UpdateConf (void *p_pvPlugin)
     SetCmd (poGUI->wTF_Cmd, poPlugin);
     SetLabel (poGUI->wTF_Title, poPlugin);
     SetMonitorFont (poPlugin);
-    /* Force to regenerate a timer */
-    poPlugin->iTimerId = 0;
+    /* Restart timer */
+    if (poPlugin->iTimerId) {
+        g_source_remove (poPlugin->iTimerId);
+        poPlugin->iTimerId = 0;
+    }
+    SetTimer(p_pvPlugin);
 }/* UpdateConf() */
 
 /**************************************************************/
@@ -829,8 +833,6 @@ static void genmon_construct (XfcePanelPlugin *plugin)
     gtk_container_add (GTK_CONTAINER (plugin), genmon->oMonitor.wEventBox);
 
     SetMonitorFont (genmon);
-
-    SetTimer (genmon);
 
     g_signal_connect (plugin, "free-data", G_CALLBACK (genmon_free), genmon);
 

commit 78e457ae136015a7cc2260bb4e95cc14307f450e

Fix genmon not being initialized on startup (regression introduced by rev 5d9618659709).  Closes: #7449.

panel-plugin/main.c
@@ -835,6 +835,7 @@ static void genmon_construct (XfcePanelPlugin *plugin)
     gtk_container_add (GTK_CONTAINER (plugin), genmon->oMonitor.wEventBox);
 
     SetMonitorFont (genmon);
+    SetTimer (genmon);
 
     g_signal_connect (plugin, "free-data", G_CALLBACK (genmon_free), genmon);
 