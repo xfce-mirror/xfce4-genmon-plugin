2d5fa307827a7b32af4f7b2ae0604f943e3a6e99
John Lindgren <john.lindgren@aol.com>
2012-08-21 19:33:45 -0400

    Allow timer period to be set at 250ms resolution.  Patch from Miguel Guedes.  Closes: #9102.

--- panel-plugin/config_gui.c
+++ panel-plugin/config_gui.c
@@ -106,8 +106,8 @@ int genmon_CreateConfigGUI (GtkWidget *vbox1,
     gtk_widget_show (alignment1);
     gtk_container_add (GTK_CONTAINER (eventbox1), alignment1);
 
-    wSc_Period_adj = gtk_adjustment_new (15, 1, 60*60*24, 1, 1, 0);
-    wSc_Period = gtk_spin_button_new (GTK_ADJUSTMENT (wSc_Period_adj), 1, 0);
+    wSc_Period_adj = gtk_adjustment_new (15, .25, 60*60*24, .25, 1, 0);
+    wSc_Period = gtk_spin_button_new (GTK_ADJUSTMENT (wSc_Period_adj), .25, 2);
     gtk_widget_show (wSc_Period);
     gtk_container_add (GTK_CONTAINER (alignment1), wSc_Period);
     gtk_tooltips_set_tip (tooltips, wSc_Period,
