2d428d94a52c8801266dd09e0ea91b7ada7fe40d
ToZ <tony.paulic@gmail.com>
2019-10-04 18:52:14 -0400

    Allow compilation with panel 4.15

--- panel-plugin/config_gui.c
+++ panel-plugin/config_gui.c
@@ -29,7 +29,7 @@
 
 #include <libxfce4util/libxfce4util.h>
 #include <libxfce4ui/libxfce4ui.h>
-#include <libxfce4panel/xfce-panel-plugin.h>
+#include <libxfce4panel/libxfce4panel.h>
 
 
 #define COPYVAL(var, field)    ((var)->field = field)
--- panel-plugin/config_gui.h
+++ panel-plugin/config_gui.h
@@ -29,7 +29,7 @@
 #include <gtk/gtk.h>
 
 #include <libxfce4util/libxfce4util.h>
-#include <libxfce4panel/xfce-panel-plugin.h>
+#include <libxfce4panel/libxfce4panel.h>
 
 
 typedef struct gui_t {
